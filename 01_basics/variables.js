384ba8e327ed32439ca59428a860c15400fe665c
Hridoy Mia <109734378+hridoymia333@users.noreply.github.com>
2025-06-11 07:43:30 +0000

    create a JS variable file to practice let, const, and var usage

---
+++ 01_basics/variables.js
@@ -0,0 +1,25 @@
+//Playing with variables.
+/*
+Js provides various ways to declare variables.such as:
+1. Const - unchangeable 
+2. let - changeable
+3. var- oldschool version presently not used.
+4. without keyword - forbidden 
+*/
+
+// Declaring variables for containing an user login information.
+
+const userId = 23544564 
+let userName = "jubairhridoy333" // string types data are written between inverted comma.
+var userRegion = "Bangladesh"
+userDivison = "Dhaka"
+//Varialbles name are written using camelCase method.
+
+//priting syntax in Js
+console.log(userId);
+console.log(userName);
+console.log(userId, userName, userRegion, userDivison); //print all variable at once.
+
+console.table([userId, userName, userRegion, userDivison]) //console.table is a built-in JavaScript method that displays tabular data in a neatly formatted table in the console. 
+
+
